9387eff0381766e51a4731fab28680f31649afd1
Željko Filipin <zeljko.filipin@gmail.com>
2014-10-23 17:09:40 +0200

    Fixed RuboCop offense Style/ClassAndModuleChildren
    $ bundle exec rubocop
    Inspecting 11 files
    ..........C
    
    Offenses:
    
    spec/support/request_helpers.rb:1:8: C: Use nested module/class
    definitions instead of compact style.
    module MediawikiApi::RequestHelpers
           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    
    11 files inspected, 1 offense detected
    
    Bug: 63307
    Change-Id: Id4858bf4402526563757aaa7a3257e68918487f2

--- spec/support/request_helpers.rb
+++ spec/support/request_helpers.rb
@@ -1,34 +1,36 @@
-module MediawikiApi::RequestHelpers
-  def api_url
-    'http://localhost/api.php'
-  end
+module MediawikiApi
+  module RequestHelpers
+    def api_url
+      'http://localhost/api.php'
+    end
 
-  def index_url
-    'http://localhost/w/index.php'
-  end
+    def index_url
+      'http://localhost/w/index.php'
+    end
 
-  def mock_token
-    'token123'
-  end
+    def mock_token
+      'token123'
+    end
 
-  def stub_api_request(method, params)
-    params = params.each.with_object({}) { |(k, v), p| p[k] = v.to_s }
+    def stub_api_request(method, params)
+      params = params.each.with_object({}) { |(k, v), p| p[k] = v.to_s }
 
-    stub_request(method, api_url).
-      with((method == :post ? :body : :query) => params.merge(format: 'json'))
-  end
+      stub_request(method, api_url).
+        with((method == :post ? :body : :query) => params.merge(format: 'json'))
+    end
 
-  def stub_action_request(action, params = {})
-    method = params.delete(:http_method) || :post
+    def stub_action_request(action, params = {})
+      method = params.delete(:http_method) || :post
 
-    stub_api_request(method, params.merge(action: action, token: mock_token))
-  end
+      stub_api_request(method, params.merge(action: action, token: mock_token))
+    end
 
-  def stub_token_request(type, warning = nil)
-    response = { tokens: { "#{type}token" => mock_token } }
-    response[:warnings] = { type => { '*' => [warning] } } unless warning.nil?
+    def stub_token_request(type, warning = nil)
+      response = { tokens: { "#{type}token" => mock_token } }
+      response[:warnings] = { type => { '*' => [warning] } } unless warning.nil?
 
-    stub_api_request(:get, action: 'tokens', type: type).
-      to_return(body: response.to_json)
+      stub_api_request(:get, action: 'tokens', type: type).
+        to_return(body: response.to_json)
+    end
   end
 end
