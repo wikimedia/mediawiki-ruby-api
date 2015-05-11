8f1aa2fa1a771696c37116ce008f227b1fa2a4ce
ragesoss <ragesoss@gmail.com>
2015-05-10 22:33:04 -0700

    copyedit exceptions spec

--- spec/exceptions_spec.rb
+++ spec/exceptions_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe MediawikiApi::ApiError do
-  it 'should create a generic ApiError when initiated with no data' do
+  it 'should create a generic ApiError when initialized with no data' do
     mock_error = MediawikiApi::ApiError.new
     expect(mock_error).to be_a(MediawikiApi::ApiError)
   end
